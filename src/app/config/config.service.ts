9cbac5de2ba2eff7869c2558da4f253f65fa5c28
hong-il <clroyan01@gmail.com>
2020-02-27 16:18:58 +0900

    Update service - get config json

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -5,6 +5,11 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ConfigService {
+  configUrl = 'assets/config.json';
 
   constructor(private http: HttpClient) { }
+
+  getConfig() {
+    return this.http.get(this.configUrl);
+  }
 }
