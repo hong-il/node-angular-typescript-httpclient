98d7b7b0aa1e103c269cece824025cc7b2ef0c52
hong-il <clroyan01@gmail.com>
2020-03-04 17:12:07 +0900

    Create domain - hero

--- src/app/heroes/heroes.service.ts
+++ src/app/heroes/heroes.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Hero } from './hero';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'my-auth-token'
+  })
+};
+
+@Injectable()
 export class HeroesService {
+  heroesUrl = 'api/heroes'; 
 
-  constructor() { }
+  constructor(
+    private http: HttpClient) { }
 }
