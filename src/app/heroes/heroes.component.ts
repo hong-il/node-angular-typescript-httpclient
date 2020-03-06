ec8eb8d9749904a6339bcc5c4abfdeef9feced83
hong-il <clroyan01@gmail.com>
2020-03-06 15:09:37 +0900

    Update heroes component - crud

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Hero } from './hero';
+import { HeroesService } from './heroes.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  providers: [HeroesService],
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  editHero: Hero;
 
-  constructor() { }
+  constructor(private heroesService: HeroesService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroesService.getHeroes()
+      .subscribe(heroes => (this.heroes = heroes));
+  }
+
+  add(name: string): void {
+    this.editHero = undefined;
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+
+    const newHero: Hero = { name } as Hero;
+    this.heroesService
+      .addHero(newHero)
+      .subscribe(hero => this.heroes.push(hero));
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroesService
+      .deleteHero(hero.id)
+      .subscribe();
+  }
+
+  edit(hero: Hero) {
+    this.editHero = hero;
+  }
+
+  search(searchTerm: string) {
+    this.editHero = undefined;
+    if (searchTerm) {
+      this.heroesService
+        .searchHeroes(searchTerm)
+        .subscribe(heroes => (this.heroes = heroes));
+    }
+  }
+
+  update() {
+    if (this.editHero) {
+      this.heroesService
+        .updateHero(this.editHero)
+        .subscribe(hero => {
+        const ix = hero ? this.heroes.findIndex(h => h.id === hero.id) : -1;
+        if (ix > -1) {
+          this.heroes[ix] = hero;
+        }
+      });
+      this.editHero = undefined;
+    }
+  }
 }
