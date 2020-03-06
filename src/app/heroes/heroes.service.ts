99288d47e053d433aa6f6e145b7db9a42be25d99
hong-il <clroyan01@gmail.com>
2020-03-06 15:08:08 +0900

    Create service - heroes crud

--- src/app/heroes/heroes.service.ts
+++ src/app/heroes/heroes.service.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 
+
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 import { Hero } from './hero';
+import { HttpErrorHandler, HandleError } from '../http-error-handler.service';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -16,8 +18,56 @@ const httpOptions = {
 
 @Injectable()
 export class HeroesService {
-  heroesUrl = 'api/heroes'; 
+  heroesUrl = 'api/heroes';  // URL to web api
+  private handleError: HandleError;
 
   constructor(
-    private http: HttpClient) { }
+    private http: HttpClient,
+    httpErrorHandler: HttpErrorHandler) {
+    this.handleError = httpErrorHandler.createHandleError('HeroesService');
+  }
+
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError('getHeroes', []))
+      );
+  }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+    term = term.trim();
+
+    const options = term ?
+     { params: new HttpParams().set('name', term) } : {};
+
+    return this.http.get<Hero[]>(this.heroesUrl, options)
+      .pipe(
+        catchError(this.handleError<Hero[]>('searchHeroes', []))
+      );
+  }
+
+  addHero (hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions)
+      .pipe(
+        catchError(this.handleError('addHero', hero))
+      );
+  }
+
+  deleteHero (id: number): Observable<{}> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, httpOptions)
+      .pipe(
+        catchError(this.handleError('deleteHero'))
+      );
+  }
+
+  updateHero (hero: Hero): Observable<Hero> {
+    httpOptions.headers =
+      httpOptions.headers.set('Authorization', 'my-new-auth-token');
+
+    return this.http.put<Hero>(this.heroesUrl, hero, httpOptions)
+      .pipe(
+        catchError(this.handleError('updateHero', hero))
+      );
+  }
 }
