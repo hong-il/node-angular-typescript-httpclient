ab846048b6b6b12aff7788d2bd80128fe23cb7d1
hong-il <clroyan01@gmail.com>
2020-03-10 14:14:28 +0900

    Update service - downloader

--- src/app/downloader/downloader.service.ts
+++ src/app/downloader/downloader.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { tap } from 'rxjs/operators';
+
+import { MessageService } from '../message.service';
+
+@Injectable()
 export class DownloaderService {
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
-  constructor() { }
+  getTextFile(filename: string) {
+    return this.http.get(filename, {responseType: 'text'})
+      .pipe(
+        tap(
+          data => this.log(filename, data),
+          error => this.logError(filename, error)
+        )
+      );
+  }
+
+  private log(filename: string, data: string) {
+    const message = `DownloaderService downloaded "${filename}" and got "${data}".`;
+    this.messageService.add(message);
+  }
+
+  private logError(filename: string, error: any) {
+    const message = `DownloaderService failed to download "${filename}"; got error "${error.message}".`;
+    console.error(message);
+    this.messageService.add(message);
+  }
 }
