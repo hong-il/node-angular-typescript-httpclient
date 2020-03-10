142d0aadbc94970b583bfb77064126ef886e6f37
hong-il <clroyan01@gmail.com>
2020-03-10 14:16:56 +0900

    Update downloader component - download

--- src/app/downloader/downloader.component.ts
+++ src/app/downloader/downloader.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { DownloaderService } from './downloader.service';
 
 @Component({
   selector: 'app-downloader',
   templateUrl: './downloader.component.html',
-  styleUrls: ['./downloader.component.scss']
+  providers: [ DownloaderService ]
 })
-export class DownloaderComponent implements OnInit {
+export class DownloaderComponent {
+  contents: string;
+  constructor(private downloaderService: DownloaderService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  clear() {
+    this.contents = undefined;
   }
 
+  download() {
+    this.downloaderService.getTextFile('assets/textfile.txt')
+      .subscribe(results => this.contents = results);
+  }
 }
