e16aea241423dbc8b698bf3eb53dde21055d7092
hong-il <clroyan01@gmail.com>
2020-03-26 16:59:14 +0900

    Update package search component - search

--- src/app/package-search/package-search.component.ts
+++ src/app/package-search/package-search.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+
+import { NpmPackageInfo, PackageSearchService } from './package-search.service';
+
 @Component({
   selector: 'app-package-search',
   templateUrl: './package-search.component.html',
-  styleUrls: ['./package-search.component.scss']
+  providers: [ PackageSearchService ]
 })
 export class PackageSearchComponent implements OnInit {
+  withRefresh = false;
+  packages$: Observable<NpmPackageInfo[]>;
+  private searchText$ = new Subject<string>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  search(packageName: string) {
+    this.searchText$.next(packageName);
   }
+
+  ngOnInit() {
+    this.packages$ = this.searchText$.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap(packageName =>
+        this.searchService.search(packageName, this.withRefresh))
+    );
+  }
+
+  constructor(private searchService: PackageSearchService) { }
+
+
+  toggleRefresh() { this.withRefresh = ! this.withRefresh; }
 
 }
